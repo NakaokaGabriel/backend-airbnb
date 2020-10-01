489efeca60d91b227b47110451a587cc7d3f357e
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-09-30 22:55:45 -0300

    Revert "Revert "Revert "master: config project"""
    This reverts commit b9ea9ca659a520dfeeaa0613182f4bf608ab4f68.

--- src/shared/domain/http/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const routes = Router();
-
-routes.get('/', (request, response) => {
-  return response.send('<h1>Hello world</h1>');
-});
-
-export default routes;
