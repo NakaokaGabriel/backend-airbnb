c9131e580b787f06b81aa0fcfe854cbabe7bba86
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-10-04 20:13:49 -0300

    feature: config folders and modules of host

--- public/src/shared/domain/database/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var knex_1 = __importDefault(require("knex"));
-var knex_2 = __importDefault(require("@config/knex"));
-var connection = knex_1.default(knex_2.default);
-exports.default = connection;
--- public/src/shared/domain/http/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var routes = express_1.Router();
-routes.get('/', function (request, response) {
-    return response.send('<h1>Hello world</h1>');
-});
-exports.default = routes;
