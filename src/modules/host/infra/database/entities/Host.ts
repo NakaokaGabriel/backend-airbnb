06553fab35ff3d99f0a177e42002c928dda4f8dd
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-10-09 16:07:07 -0300

    feature: create and config configs on host database

--- src/modules/host/infra/database/entities/Host.ts
+++ src/modules/host/infra/database/entities/Host.ts
@@ -1,15 +1,37 @@
-export class Host {
-  readonly id: string;
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity('hosts')
+export class Host {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
   name: string;
 
+  @Column()
   email: string;
 
+  @Column()
   password: string;
 
+  @Column('boolean')
   host_verify: boolean;
 
+  @Column('boolean')
   host_type: boolean;
 
+  @Column('int')
   stars: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
