6c8bb38679d8abf5ade4c7f47b46f6d530de0b54
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-10-10 20:20:23 -0300

    feature: create hash provider and create hash to hosts

---
+++ src/shared/containers/providers/hash/fakes/FakeHash.ts
@@ -0,0 +1,11 @@
+import { IHashRepository } from '../repository/IHashRepository';
+
+export class FakeHash implements IHashRepository {
+  public async generateHash(payload: string): Promise<string> {
+    return payload;
+  }
+
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
+    return payload === hashed;
+  }
+}
