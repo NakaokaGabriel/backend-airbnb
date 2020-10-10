fb086ebd8428e1557087c6936ee51ac45b6bbce1
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-10-10 12:30:15 -0300

    feature: create host repository with typeorm

--- src/modules/host/infra/database/repositories/HostRepository.ts
+++ src/modules/host/infra/database/repositories/HostRepository.ts
@@ -1,11 +1,32 @@
+import { getRepository, Repository } from 'typeorm';
+
 import { IHostRepository } from '@modules/host/repositories/IHostRepository';
 import { ICreateHostDTO } from '@modules/host/dtos/ICreateHostDTO';
+
 import { Host } from '../entities/Host';
 
 export class HostRepositories implements IHostRepository {
-  constructor() {}
+  private ormRepository: Repository<Host>;
 
-  public async create(data: ICreateHostDTO): Promise<Host> {}
+  constructor() {
+    this.ormRepository = getRepository(Host);
+  }
 
-  public async findByEmail(email: string): Promise<Host | undefined> {}
+  public async create(data: ICreateHostDTO): Promise<Host> {
+    const host = await this.ormRepository.create(data);
+
+    await this.ormRepository.save(host);
+
+    return host;
+  }
+
+  public async findByEmail(email: string): Promise<Host | undefined> {
+    const hostEmail = await this.ormRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return hostEmail;
+  }
 }
