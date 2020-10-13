974b31c5a5615fcd4361e4f5c8ed831aa1d64c57
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-10-13 17:10:42 -0300

    task: adjusted error in typeorm entities paths

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -7,6 +7,6 @@ import usersRouter from '@modules/users/infra/http/routes/users.routes';
 const routes = Router();
 
 routes.use('/hosts', hostRouter);
-// routes.use('/users', usersRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
